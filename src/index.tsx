8cc8a3e787072ab1dd5c9e30ebd9b36693c48bf7
lukhanin.viacheslav <vyaches.lukhanin@x5.ru>
2025-01-07 07:27:28 +0300

    tg disable vertical swipes

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 const tg = window.Telegram.WebApp;
 tg.expand();
+tg.disableVerticalSwipes();
 
 root.render(
     <React.StrictMode>
